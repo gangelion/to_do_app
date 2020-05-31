08777cb02db3a45c7bfce055e06491cd483bd2a9
gangelion <gangelion8@gmail.com>
2020-05-31 16:42:12 +0900

    to_do_updateまで

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -2,4 +2,35 @@ class ToDosController < ApplicationController
   def index
     @to_dos = ToDo.all
   end
+
+  def new
+    @to_do = ToDo.new
+  end
+
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save
+      redirect_to to_dos_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @to_do = ToDo.find_by(id: params[:id])
+  end
+
+  def update
+    @to_do = ToDo.find_by(id: params[:id])
+    if @to_do.update(to_do_params)
+      redirect_to to_dos_url
+    else
+      render :edit
+    end
+  end
+
+  private
+  def to_do_params
+    params.require(:to_do).permit(:title)
+  end
 end
